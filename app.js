2886652b02c84aa3b61a05e6fe95829e42ea5998
ema <lotofacundo6@gmail.com>
2022-06-01 13:56:39 -0300

    update

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'public')));
 app.use(express.static(path.join(__dirname, 'views')));
 
 app.get("/", (req, res) => {
--- controller/upload/upload.js
+++ controller/upload/upload.js
@@ -2,12 +2,12 @@
 const multer = require('multer');//instalamos multer
 const storage = multer.diskStorage({ //esta es un objeto con dos propiedades ambas tiene dos funciones
     destination: function (req, file, cb) {
-        cb(null, './public/img/'); //cb es una funcion donde en destination ponemos la ruta donde se guardaran las imagenes
+        cb(null,"./public/img/"); //cb es una funcion donde en destination ponemos la ruta donde se guardaran las imagenes
     },
     filename: function (req, file, cb) {
         let ext = file.originalname.split('.').pop();
         cb(null, file.fieldname + '-' + Date.now() + '.' + 'jpg');
-        console.log(file.fieldname);
+        console.log(req.body);
     }
 })
 const upload = multer({ storage: storage, limits: { fieldSize: 10 * 1024 * 1024 } }).single('image');
